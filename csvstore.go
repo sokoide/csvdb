2d69c6d94af8b032ba6ae9fa2099218278b42706
So Koide <sokoide@gmail.cojm>
2025-08-17 11:24:22 +0900

    refactor: memory utilization of indices

--- csvstore.go
+++ csvstore.go
@@ -11,7 +11,8 @@ type Record map[string]string // filele name -> value
 
 type CSVStore struct {
 	headers []string
-	indices map[string]map[string]Record // field name -> (value -> record)
+	records []Record
+	indices map[string]map[string]int // field name -> (value -> record index)
 }
 
 func NewCSVStoreFromReader(r io.Reader) (*CSVStore, error) {
@@ -30,11 +31,12 @@ func NewCSVStoreFromReader(r io.Reader) (*CSVStore, error) {
 	for i, h := range rows[0] {
 		headers[i] = strings.TrimSpace(h)
 	}
-	indices := make(map[string]map[string]Record)
+	indices := make(map[string]map[string]int)
 	for _, h := range headers {
-		indices[h] = make(map[string]Record)
+		indices[h] = make(map[string]int)
 	}
 
+	records := make([]Record, 0, len(rows)-1)
 	for _, row := range rows[1:] {
 		rec := make(Record)
 		for i, val := range row {
@@ -42,12 +44,14 @@ func NewCSVStoreFromReader(r io.Reader) (*CSVStore, error) {
 				rec[headers[i]] = strings.TrimSpace(val)
 			}
 		}
+		records = append(records, rec)
+		recordIdx := len(records) - 1
 		for _, h := range headers {
-			indices[h][rec[h]] = rec
+			indices[h][rec[h]] = recordIdx
 		}
 	}
 
-	return &CSVStore{headers, indices}, nil
+	return &CSVStore{headers, records, indices}, nil
 }
 
 func (s *CSVStore) Lookup(field string, value string) (Record, bool) {
@@ -55,6 +59,9 @@ func (s *CSVStore) Lookup(field string, value string) (Record, bool) {
 	if !ok {
 		return nil, false
 	}
-	rec, found := idx[value]
-	return rec, found
+	recordIdx, found := idx[value]
+	if !found {
+		return nil, false
+	}
+	return s.records[recordIdx], true
 }
